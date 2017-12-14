1ca79e42765c50ef586f7ed4930edbcb80901029
davidquero <jose.david.quero.sanchez@gmail.com>
2017-12-14 14:18:55 +0100

    prueba branch1

--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -14,7 +14,7 @@ namespace WindowsFormsApp3
     {
         public Form1()
         {
-            InitializeComponent(); // comento
+            InitializeComponent(); // comento desde branch1
         }
     }
 }
