9f02a0d73546a835e8905d8bf4b9d7bd642964dd
davidquero <jose.david.quero.sanchez@gmail.com>
2018-01-08 09:23:33 +0100

    Cambio con push

--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -14,7 +14,7 @@ namespace WindowsFormsApp3
     {
         public Form1()
         {
-            InitializeComponent(); // comento desde Rama1 otra vez
+            InitializeComponent(); // comento desde Rama1 otra vez con push
         }
     }
 }
